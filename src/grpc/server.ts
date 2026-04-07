b7a274c52b70a7dc2a3c12e2bd74af20a25322ff
aneeshpatne <aneeshpatne@gmail.com>
2026-04-07 05:36:20 +0000

    Implement initial gRPC server setup and define sendEmailHandler function

--- src/grpc/server.ts
+++ src/grpc/server.ts
@@ -1,3 +1,17 @@
 import * as grpc from "@grpc/grpc-js";
 import * as protoLoader from "@grpc/proto-loader";
 import path from "path";
+import { sendHtmlMail } from "../mail/send-html-mail";
+
+const PROTO_PATH = path.join(__dirname, "sandesh.proto");
+
+const packageDefinition = protoLoader.loadSync(PROTO_PATH);
+
+const proto = grpc.loadPackageDefinition(packageDefinition) as any;
+
+const mailerProto = proto.sandesh;
+
+async function sendEmailHandler(
+  call: grpc.ServerUnaryCall<any, any>,
+  callback: grpc.sendUnaryData<any>,
+) {}
