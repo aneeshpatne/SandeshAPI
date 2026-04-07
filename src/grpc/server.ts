09a745d8fafa2896b22de454fb4581b25ad53533
aneeshpatne <aneeshpatne@gmail.com>
2026-04-07 05:31:09 +0000

    Refactor sendHtmlMail function to include app_id in subject and add gRPC server setup

---
+++ src/grpc/server.ts
@@ -0,0 +1,3 @@
+import * as grpc from "@grpc/grpc-js";
+import * as protoLoader from "@grpc/proto-loader";
+import path from "path";
--- src/mail/send-html-mail.ts
+++ src/mail/send-html-mail.ts
@@ -1,16 +1,22 @@
 import { transporter } from "./mail";
 
 type SendHtmlMailParams = {
+  app_id: string;
   to: string[];
   subject: string;
   html: string;
 };
 
-export async function sendHtmlMail({ to, subject, html }: SendHtmlMailParams) {
+export async function sendHtmlMail({
+  app_id,
+  to,
+  subject,
+  html,
+}: SendHtmlMailParams) {
   return transporter.sendMail({
     from: process.env.EMAIL_FROM,
     to,
-    subject,
+    subject: "[" + app_id + "]" + subject,
     html,
   });
 }
