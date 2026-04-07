325182bd964b6eddb1d487e4c64ee0eb4564820f
aneeshpatne <aneeshpatne@gmail.com>
2026-04-07 05:39:26 +0000

    Implement sendEmailHandler function and set up gRPC server for MailerService

--- src/grpc/server.ts
+++ src/grpc/server.ts
@@ -14,4 +14,31 @@ const mailerProto = proto.sandesh;
 async function sendEmailHandler(
   call: grpc.ServerUnaryCall<any, any>,
   callback: grpc.sendUnaryData<any>,
-) {}
+) {
+  try {
+    const { app_id, to, subject, body } = call.request;
+    await sendHtmlMail({ app_id, to, subject, html: body });
+    callback(null, {
+      success: true,
+    });
+  } catch (error: any) {
+    callback(null, {
+      success: false,
+    });
+  }
+}
+
+const server = new grpc.Server();
+
+server.addService(mailerProto.MailerService.service, {
+  SendEmail: sendEmailHandler,
+});
+
+server.bindAsync(
+  "0.0.0.0:50052",
+  grpc.ServerCredentials.createInsecure(),
+  (err, port) => {
+    if (err) throw err;
+    console.log(`gRPC mail server running on ${port}`);
+  },
+);
